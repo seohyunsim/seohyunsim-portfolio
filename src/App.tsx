5dcc49005a0a41e3f5d55fc09dc271f427ee643e
seohyunsim <ssh4563@naver.com>
2023-11-05 18:03:30 +0900

    디테일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -38,36 +38,31 @@ function App() {
         logoOption={logoOptionDefault}
         channels={channelOptionDefault}
       />
+
+      <Carousel
+        id="['TOP', 'akar-icons:align-to-top']"
+        width="82%"
+        isArrowShow={false}
+        isAutoplay={true}
+        transition={1}
+        autoplaySpeed={900}
+        isAutoplayControl={false}
+      >
+        <Image
+          noShowHead={true}
+          zoomWhenHover={false}
+          src="common/firstBanner.png"
+        />
+        <Image
+          noShowHead={true}
+          zoomWhenHover={false}
+          src="common/banner.png"
+        />
+      </Carousel>
+
       <IntroduceBanner>
         {/* <h3>안녕하세요</h3> */}
         <div className="wrapper">
-          <div className="detailWrap">
-            <div className="titleWrap">
-              <span className="title">
-                🔅 인터랙티브한 업무와 개발을 지향합니다.
-              </span>
-            </div>
-            <div className="des">
-              <span>
-                사용자와의 상호작용을 강조하고, 이를 통해 웹 페이지나
-                애플리케이션을 더 동적이고 매력적으로 만드는데 중점을 둡니다.
-              </span>
-              <span>
-                서비스를 개발하며 전반적인 UI/UX 구현을 담당한 경험이 있으며,
-                지속적으로 개선해 완성도를 높인 결과물로 팀원들에게도 인정받은
-                경험이 있습니다.
-              </span>
-              <Link
-                onClick={() =>
-                  window.open(
-                    "https://www.notion.so/459a959ffb74477e89b04780c901aa73?pvs=4"
-                  )
-                }
-              >
-                개발 팀원이 평가한 나
-              </Link>
-            </div>
-          </div>
           <div className="detailWrap">
             <div className="titleWrap">
               <span className="title">🏆 npm 라이브러리 개발 및 수상 경험</span>
@@ -94,6 +89,31 @@ function App() {
           </div>
           <div className="detailWrap">
             <div className="titleWrap">
+              <span className="title">🔅 인터랙티브한 개발자</span>
+            </div>
+            <div className="des">
+              <span>
+                사용자와의 상호작용을 강조하고, 이를 통해 웹 페이지나
+                애플리케이션을 더 동적이고 매력적으로 만드는데 중점을 둡니다.
+              </span>
+              <span>
+                서비스를 개발하며 전반적인 UI/UX 구현을 담당한 경험이 있으며,
+                지속적으로 개선해 완성도를 높인 결과물로 팀원들에게도 인정받은
+                경험이 있습니다.
+              </span>
+              <Link
+                onClick={() =>
+                  window.open(
+                    "https://www.notion.so/459a959ffb74477e89b04780c901aa73?pvs=4"
+                  )
+                }
+              >
+                개발 팀원이 평가한 나
+              </Link>
+            </div>
+          </div>
+          <div className="detailWrap">
+            <div className="titleWrap">
               <span className="title">💁‍♀️ 천상 프론트엔드 개발자!</span>
             </div>
             <div className="des">
@@ -109,26 +129,6 @@ function App() {
           </div>
         </div>
       </IntroduceBanner>
-      {/* <Carousel
-        id="['TOP', 'akar-icons:align-to-top']"
-        width="82%"
-        isArrowShow={false}
-        isAutoplay={true}
-        transition={1}
-        autoplaySpeed={900}
-        isAutoplayControl={false}
-      >
-        <Image
-          noShowHead={true}
-          zoomWhenHover={false}
-          src="common/firstBanner.png"
-        />
-        <Image
-          noShowHead={true}
-          zoomWhenHover={false}
-          src="common/banner.png"
-        />
-      </Carousel> */}
 
       {/* <SizeAdjustment id="['INTRO', 'fa:smile-o']">
         <Intro
@@ -413,7 +413,7 @@ const SizeAdjustment = styled.div`
   @media screen and (max-width: 768px) {
     padding: 10vw;
     h3 {
-      font-size: 8vw;
+      font-size: 5vw;
     }
   }
 `;
@@ -427,7 +427,7 @@ const TeckStackWrap = styled.div`
   }
   @media screen and (max-width: 768px) {
     h3 {
-      font-size: 8vw;
+      font-size: 5vw;
     }
   }
 `;
@@ -468,7 +468,7 @@ const GalleryWrap = styled.div`
   @media screen and (max-width: 768px) {
     padding: 10vw;
     h2 {
-      font-size: 8vw;
+      font-size: 5vw;
     }
     .detailWrap {
       .titleWrap {
@@ -478,10 +478,6 @@ const GalleryWrap = styled.div`
         .title {
           padding: 10px 0;
         }
-      }
-      .des {
-        padding: 2em 10px;
-        gap: 5px;
       }
     }
   }
@@ -541,13 +537,12 @@ const IntroduceBanner = styled.div`
       .titleWrap {
         display: flex;
         flex-direction: column;
-        align-items: center;
         .title {
           padding: 10px 0;
         }
       }
       .des {
-        padding: 2em 10px;
+        padding: 10px;
         gap: 5px;
       }
     }
